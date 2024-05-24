f99bea1c5817cd29fd4e79c0caa9b54712e1e559
Anhtuan2190 <tuanva2190@gmail.com>
2024-05-24 20:15:42 +0700

    Add interface CRUDService

--- src/main/java/com/codegym/c07blog/service/CRUDService.java
+++ src/main/java/com/codegym/c07blog/service/CRUDService.java
@@ -1,4 +1,11 @@
 package com.codegym.c07blog.service;
 
-public interface CRUDService {
+import java.util.List;
+import java.util.UUID;
+
+public interface CRUDService <T>{
+    T findById(UUID id);
+    List<T> findAll();
+    void save(T t);
+    void deleteById (UUID id);
 }
