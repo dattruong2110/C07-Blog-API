9391d2f653f303ccff9969db4819e2e2828c6bc3
DESKTOP-RCFES7K\Admin <nhng1215@gmail.com>
2024-05-29 21:15:17 +0700

    update blog entity 2

--- src/main/resources/db/migration/V1_00_Create_table_blog.sql
+++ src/main/resources/db/migration/V1_00_Create_table_blog.sql
@@ -4,12 +4,12 @@ CREATE TABLE category (
 );
 
 CREATE TABLE blog (
-    id binary(16) PRIMARY KEY,
-    title VARCHAR(255) NOT NULL ,
-    content varchar(255) not null ,
-    user varchar(255),
-    category_id binary(16),
-    foreign key (category_id) references category(id)
+                      id binary(16) PRIMARY KEY,
+                      title VARCHAR(255) NOT NULL ,
+                      content varchar(255) not null ,
+                      user varchar(255),
+                      category_id binary(16),
+                      foreign key (category_id) references category(id)
 );
 
 insert into category(id, name)
