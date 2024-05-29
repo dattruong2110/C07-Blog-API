4a34302e6a77d852ea06c2c42c978f7e4205c055
Trương Tấn Đạt <tandattruong2110@gmail.com>
2024-05-29 22:30:11 +0700

    Create function createAdminAccount

--- src/main/java/com/codegym/c07blog/service/impl/RoleService.java
+++ src/main/java/com/codegym/c07blog/service/impl/RoleService.java
@@ -1,14 +1,21 @@
 package com.codegym.c07blog.service.impl;
 
 import com.codegym.c07blog.entity.authentication.Role;
+import com.codegym.c07blog.entity.authentication.User;
+import com.codegym.c07blog.entity.authentication.UserRole;
 import com.codegym.c07blog.repository.IRoleRepository;
+import com.codegym.c07blog.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class RoleService {
     @Autowired
     private IRoleRepository roleRepository;
+    @Autowired
+    private IUserRepository userRepository;
 
     public Role saveRole(Role role) throws Exception {
         if (role.isSuperAdmin()) {
@@ -18,5 +25,18 @@ public class RoleService {
             }
         }
         return roleRepository.save(role);
+    }
+
+    public boolean isUserSuperAdmin(UUID userId) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return false;
+        }
+        for (UserRole userRole : user.getUserRole()) {
+            if (userRole.getRole().isSuperAdmin()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
